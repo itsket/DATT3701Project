4a58abb874f70ff8f012ede6ff7262d8efcc42d3
itsket <ket18@my.yorku.ca>
2024-01-31 12:11:50 -0500

    pick up mech setup (not working yet)

---
+++ Egress/Assets/Scrpts/Pickup.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool istriggered= false;
+        
+        if 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
