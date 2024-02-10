90f043f3a43c1d7bdd5263ac40a8c56a483a6cba
itsket <ket18@my.yorku.ca>
2024-02-10 15:01:13 -0500

    PickUp Mech working
    E to Pickup F to Drop

--- Egress/Assets/Scripts/PickupKey.cs
+++ Egress/Assets/Scripts/PickupKey.cs
@@ -9,11 +9,39 @@ public class PickupKey : MonoBehaviour
 
     void Start()
     {
-        Key.GetComponent<Rigidbody>().isKinemtic = true;
+        Key.GetComponent<Rigidbody>().isKinematic = true;
     }
 
     void Update()
     {
-        
+      if(Input.GetKey(KeyCode.F))  {
+        Drop();
+      }
     }
+    void Drop()
+    {
+        Keyarea.DetachChildren();
+        Key.transform.eulerAngles = new Vector3(Key.transform.position.x, Key.transform.position.z, Key.transform.position.y);
+        Key.GetComponent<Rigidbody>().isKinematic = false;
+        Key.GetComponent<MeshCollider>().enabled = true;
+    }
+    void Pickup()
+    {
+        Key.GetComponent<Rigidbody>().isKinematic = true;
+        Key.transform.position = Keyarea.transform.position;
+        Key.transform.rotation = Keyarea.transform.rotation;
+
+        Key.GetComponent<MeshCollider>().enabled = false;
+        Key.transform.SetParent(Keyarea);
+
+
+    }
+   private void OnTriggerStay(Collider other)
+   {
+    if(other.gameObject.tag == "Player"){
+        if(Input.GetKey(KeyCode.E)){
+            Pickup();
+        }
+    }
+   }
 }
