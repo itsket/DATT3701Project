1d281bc7039a05ba4f8e5cf453a4868b651498fc
Tom Zhang <hothalftom@gmail.com>
2024-03-08 21:36:23 -0500

    Room 1

--- Egress/Assets/Scrpts/OpenDoor.cs
+++ Egress/Assets/Scrpts/OpenDoor.cs
@@ -8,12 +8,21 @@ public class OpenDoor : MonoBehaviour
     public bool isUnlocked = false;
     public GameObject moveto;
 
+    public Animator anim;
+
+
     // Update is called once per frame
     void Update()
     {
         if (isUnlocked) {
-            float step = 3f * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, moveto.transform.position, step);
+
+            anim.SetBool("Key", true);
+
         }
+        else
+        {
+            anim.SetBool("Key", false);
+        }
+
     }
 }
