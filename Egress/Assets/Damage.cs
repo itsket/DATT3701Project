627cced8beecc95b2ea029205de419df21e2f862
Zed <zimikejiani@gmail.com>
2024-03-10 09:49:56 -0400

    c

--- Egress/Assets/Damage.cs
+++ Egress/Assets/Damage.cs
@@ -10,6 +10,8 @@ public class Damage : MonoBehaviour
     public GameObject turret2;
     public GameObject door1;
     public GameObject door2;
+    public GameObject door3;
+    public GameObject door4;
     public GameObject respawn;
     public void Die()
     {
@@ -20,5 +22,9 @@ public class Damage : MonoBehaviour
     public void ResetRoom() { 
     turret1.GetComponent<Turret>() .enabled = false;
         turret2.GetComponent<Turret>().enabled = false;
+        door1.GetComponent<OpenDoor>().isUnlocked = true;
+        door2.GetComponent<OpenDoor>().isUnlocked = true;
+        door3.GetComponent<OpenDoor>().isUnlocked = true;
+        door4.GetComponent<OpenDoor>().isUnlocked = true;
     }
 }
--- Egress/Assets/Wethermon.cs
+++ Egress/Assets/Wethermon.cs
@@ -6,7 +6,12 @@ public class Wethermon : MonoBehaviour
 {
     public GameObject turret1;
     public GameObject turret2;
+    public GameObject door1;
+    public GameObject door2;
+    public GameObject door3;
+    public GameObject door4;
     private bool hasCountedToSeconds;
+    private bool completed =false;
     private float timer;
     public float countToSeconds;
     public GameObject player;
@@ -30,13 +35,26 @@ public class Wethermon : MonoBehaviour
         turret1.GetComponent<Turret>().enabled = false;
         turret2.GetComponent<Turret>().enabled = false;
         Debug.Log("Counted to " + countToSeconds + " seconds!");
+        door1.GetComponent<OpenDoor>().isUnlocked = true;
+        door2.GetComponent<OpenDoor>().isUnlocked = true;
+        door3.GetComponent<OpenDoor>().isUnlocked = true;
+        door4.GetComponent<OpenDoor>().isUnlocked = true;
+        completed = true;
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag.Equals("Player")){ 
+      
+        if (other.tag.Equals("Player") && completed == false){ 
             turret1.GetComponent<Turret>().enabled = true;
             turret2.GetComponent<Turret>().enabled=true;
-         
+
+            door1.GetComponent<OpenDoor>().isUnlocked = false;
+            door2.GetComponent<OpenDoor>().isUnlocked = false;
+            door3.GetComponent<OpenDoor>().isUnlocked = false;
+            door4.GetComponent<OpenDoor>().isUnlocked = false;
+       
         }
+
+     
     }
 }
