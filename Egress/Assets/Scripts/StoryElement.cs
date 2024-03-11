d5d493ead373afddb7291d898cb6a3e18622df73
Zed <zimikejiani@gmail.com>
2024-03-10 21:48:49 -0400

    n

--- Egress/Assets/Scripts/StoryElement.cs
+++ Egress/Assets/Scripts/StoryElement.cs
@@ -23,7 +23,7 @@ public class StoryElement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-      
+      if(other.tag.Equals("Player"))
         TriggerDialogueOnEnter();
        
     }
